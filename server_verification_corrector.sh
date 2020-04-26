2dc6b9dbab037c0141099fc81cbc70a40dced032
Tontonjo <60965766+Tontonjo@users.noreply.github.com>
2020-04-26 11:29:52 +0200

    Create server_verification_corrector.sh

---
+++ server_verification_corrector.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Tonton Jo - 2020
+# Join me on Youtube: https://www.youtube.com/channel/UCnED3K6K5FDUp-x_8rwpsZw
+
+# This script aims to correct  server certificate verification failed i encoutered against a valid let's encrypt certificate
+
+#USAGE:
+
+#Sources: https://fabianlee.org/2019/01/28/git-client-error-server-certificate-verification-failed/
+
+varversion=1.0
+#V1.0: Initial Release
+
+
+echo "----------------------------------------------------------------"
+echo "Tonton Jo - 2020"
+echo "Server certificate verification failed corrector V$varversion"
+echo "This script will add a pem file for every entry and declar it in /etc/ssl/certs/ca-certificates.crt"
+echo "----------------------------------------------------------------"
+
+echo "Target Hostname? (hostname.domain.tld): "
+read 'varhostname'
+echo "Target port? (443): "
+read 'varport'
+
+echo "- Installing - updating dependencies"
+sudo apt-get install apt-transport-https ca-certificates -y
+sudo update-ca-certificates
+echo "- Creating .pem for $varhostname:$varport"
+openssl s_client -showcerts -servername $varhostname -connect $varhostname:$varport </dev/null 2>/dev/null | sed -n -e '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p'  > $varhostname.pem
+
+cat $varhostname.pem | sudo tee -a /etc/ssl/certs/ca-certificates.crt
+
+echo "- Done!"
