3e1a04b8aaf3c05441a6999d28897501fa2b125b
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2024-05-02 10:06:57 +0200

    V1.1
    # V1.1: Some enhancements, use variables for country, now count total of banned IP's

--- geoip/geoiplookup.sh
+++ geoip/geoiplookup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Tonton Jo - 2024
+# Join me on Youtube: https://www.youtube.com/c/tontonjo
+
+# V1.1: Some enhancements, use variables for country, now count total of banned IP's
+# V1.0: Initial Release
+
+
+# Settings
+country_to_display=Switzerland
+
 # Vérification si geoiplookup est installé
 if ! command -v geoiplookup &> /dev/null; then
     echo "geoiplookup n'est pas installé. Veuillez l'installer pour exécuter ce script."
     exit 1
 fi
 
-# Exécution de la commande fail2ban-client pour obtenir la liste des adresses IP bannies
+# Exécution de la commande fail2ban-client pour obtenir la liste des adresses IP bannies et définir leur nombre
 banned_ips=$(fail2ban-client status npm-docker | grep -A 1000 "Banned IP list:" | cut -d ":" -f2-)
-
+numberofbannedips=$(echo "$banned_ips" | wc -w)
 # Initialisation d'un tableau associatif pour stocker les comptages par pays
 declare -A counts
 declare -A ip_by_country
@@ -33,8 +43,12 @@ Statistiques:
 for country in "${!counts[@]}"; do
     echo "$country: ${counts[$country]}"
 done | sort -t ':' -k2nr
-# Affichage des adresses IP pour la Suisse
+
+# Affichage des Résultats
 echo "
-Adresses IP pour la Suisse:
+Total des IP bannies: $numberofbannedips
+
+Adresses IP pour $country_to_display:
+${ip_by_country["$country_to_display"]}
+
 "
-echo "${ip_by_country['Switzerland']}"
